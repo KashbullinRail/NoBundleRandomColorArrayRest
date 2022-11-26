6652a2f20a3a6877b76b8cccdcd92f1d3b996f7b
KashbullinRail <kashbullinrail@gmail.com>
2022-11-26 16:25:02 +0300

    создана класс Squares.kt

---
+++ app/src/main/java/com/example/nobundlerandomcolorarrayrest/Squares.kt
@@ -0,0 +1,16 @@
+package com.example.nobundlerandomcolorarrayrest
+
+class Squares private constructor(
+    val size: Int,
+    val colors: List<Int>
+) {
+
+    constructor(
+        size: Int,
+        colorProducer:() -> Int
+    ):this(
+        size = size,
+        colors = (1..size*size).map { colorProducer() }
+    )
+
+}
